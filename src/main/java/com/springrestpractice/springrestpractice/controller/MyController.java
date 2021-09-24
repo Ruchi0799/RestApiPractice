10dc9cbbe04e9897b7b686df86a5b3f74432fe8f
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-09-24 15:14:07 +0530

    [ruchi]: implemented display hello + name as path parameter using Rest Api

--- src/main/java/com/springrestpractice/springrestpractice/controller/MyController.java
+++ src/main/java/com/springrestpractice/springrestpractice/controller/MyController.java
@@ -2,6 +2,7 @@ package com.springrestpractice.springrestpractice.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,6 +31,11 @@ public class MyController {
 	@RequestMapping(value= {"/query"},method=RequestMethod.GET)
 	public String sayHello(@RequestParam(value="name")String name) {
 		return "Hello " +name+ "!";
+	}
+	
+	@GetMapping("/param/{name}")
+	public String sayHelloParam(@PathVariable String name) {
+		return "Hello "+name+"!";
 	}
 	
 	
