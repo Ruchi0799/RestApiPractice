108c5561e8881e175b4a2bbe1f4e94e555cbb4b3
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-09-24 15:02:00 +0530

    [ruchi]: implemented display hello + name as query parameter using Rest Api

--- src/main/java/com/springrestpractice/springrestpractice/controller/MyController.java
+++ src/main/java/com/springrestpractice/springrestpractice/controller/MyController.java
@@ -2,18 +2,35 @@ package com.springrestpractice.springrestpractice.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
 
 @RestController
+@RequestMapping("/helloController")
 public class MyController {
 	
-	@GetMapping("/home")
-	public String home() {
-		System.out.println("Hello!");
-		return "Welcome to courses application";
-
-}
+//	@GetMapping("/home")
+//	public String home() {
+//		System.out.println("Hello!");
+//		return "Welcome to courses application";
+//
+//}
+	
+	
+	@RequestMapping(value= {"","/","/home"})
+	public String sayHello() {
+		return "Hello World!!";
+	}
+	
+	@RequestMapping(value= {"/query"},method=RequestMethod.GET)
+	public String sayHello(@RequestParam(value="name")String name) {
+		return "Hello " +name+ "!";
+	}
+	
+	
 }
